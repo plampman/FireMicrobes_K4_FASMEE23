61060e21395108a584ebb4f990d739b9a649e290
Phin Lampman <plampman7@gmail.com>
2025-05-29 17:43:29 -0700

    Seperated calibration script

--- Script/FASMEE23/FASMEE23_Carbon.R
+++ Script/FASMEE23/FASMEE23_Carbon.R
@@ -44,121 +44,6 @@ FASMEE_CO_CO2 <- FASMEE_CO_CO2 %>%
   arrange(local_time)
 
 
-######################### Calibration equations
-#-----------------------------------------------------------------------------------------
-
-calibration_ints <- read.table('./Input_Data/FASMEE23/FASMEE_Carbon/FASMEE_CO_CO2_Calib_ints.txt', stringsAsFactors = F, sep = ',', header = T)
-
-calibration_ints <- calibration_ints %>% 
-  mutate(
-    StartTime_MT = ymd_hms(StartTime_MT, tz = 'MST7MDT'),
-    EndTime_MT = ymd_hms(EndTime_MT, tz = 'MST7MDT'),
-    calib_CO2_ppm = as.numeric(as.character(calib_CO2_ppm)),
-    calib_CO_ppm = as.numeric(as.character(calib_CO_ppm)),
-    int = seq(1, by = 2, length.out = n()))
-
-calibration_ints_seq <- pivot_longer(calibration_ints, cols = c('StartTime_MT', 'EndTime_MT'), names_to = 'StartStop', values_to = 'Time_MT')
-
-calibration_FASMEE_CO_CO2 <- FASMEE_CO_CO2 %>%
-  mutate(int = findInterval(FASMEE_CO_CO2$local_time, calibration_ints_seq$Time_MT)) %>%
-  filter(int %% 2 == 1)
-
-calibration_FASMEE_CO_CO2 <- left_join(calibration_FASMEE_CO_CO2, calibration_ints, join_by(int))
-
-# First day calibration 2023 10 08 
-
-calibration_20231008 <- calibration_FASMEE_CO_CO2 %>%
-  filter(local_time <= ymd_hms("2023-10-08 23:59:00", tz = "MST7MDT"))
-
-calibration_20231008_CO2 <- calibration_20231008 %>%
-  filter(!is.na(calib_CO2_ppm))
-
-calibration_20231008_CO <- calibration_20231008 %>%
-  filter(!is.na(calib_CO_ppm))
-
-CO2_20231008 <- lm(calib_CO2_ppm ~ CO2_.ppm., data = calibration_20231008_CO2)
-summary(CO2_20231008)$coefficients
-
-d <- expand.grid(CO2_.ppm. = seq(0, 2500), calib_CO2_ppm  = c(408, 1008, 2165))
-d$yhat <- predict(CO2_20231008, newdata = d)
-
-p <- ggplot(calibration_20231008_CO2, aes(x = CO2_.ppm., y = calib_CO2_ppm )) + 
-  theme_minimal() + geom_point() + geom_line(aes(y = yhat), data = d) 
-plot(p)
-
-CO_20231008 <- lm(calib_CO_ppm ~ CO_raw.ppm., data = calibration_20231008_CO)
-summary(CO_20231008)$coefficients
-
-d <- expand.grid(CO_raw.ppm. = seq(0, 600), calib_CO_ppm  = c(32, 260, 560))
-d$yhat <- predict(CO_20231008, newdata = d)
-
-p <- ggplot(calibration_20231008_CO, aes(x = CO_raw.ppm., y = calib_CO_ppm )) + 
-  theme_minimal() + geom_point() + geom_line(aes(y = yhat), data = d) 
-plot(p)
-
-# Second day check calibration (2 points) 2023 10 09
-
-calibration_20231009 <- calibration_FASMEE_CO_CO2 %>%
-  filter(local_time >= ymd_hms("2023-10-08 23:59:00", tz = "MST7MDT") & local_time <= ymd_hms("2023-10-09 23:59:00", tz = "MST7MDT"))
-
-calibration_20231009_CO2 <- calibration_20231009 %>%
-  filter(!is.na(calib_CO2_ppm))
-
-calibration_20231009_CO <- calibration_20231009 %>%
-  filter(!is.na(calib_CO_ppm))
-
-CO2_20231009 <- lm(calib_CO2_ppm ~ CO2_.ppm., data = calibration_20231009_CO2)
-summary(CO2_20231009)$coefficients
-
-d <- expand.grid(CO2_.ppm. = seq(0, 1500), calib_CO2_ppm  = c(408, 1008))
-d$yhat <- predict(CO2_20231009, newdata = d)
-
-p <- ggplot(calibration_20231009_CO2, aes(x = CO2_.ppm., y = calib_CO2_ppm )) + 
-  theme_minimal() + geom_point() + geom_line(aes(y = yhat), data = d) 
-plot(p)
-
-CO_20231009 <- lm(calib_CO_ppm ~ CO_raw.ppm., data = calibration_20231009_CO)
-summary(CO_20231009)$coefficients
-
-d <- expand.grid(CO_raw.ppm. = seq(0, 600), calib_CO_ppm  = c(32, 560))
-d$yhat <- predict(CO_20231009, newdata = d)
-
-p <- ggplot(calibration_20231009_CO, aes(x = CO_raw.ppm., y = calib_CO_ppm )) + 
-  theme_minimal() + geom_point() + geom_line(aes(y = yhat), data = d) 
-plot(p)
-
-# Third day calibration 2023 10 10 
-
-calibration_20231010 <- calibration_FASMEE_CO_CO2 %>%
-  filter(local_time >= ymd_hms("2023-10-09 23:59:00", tz = "MST7MDT") & local_time <= ymd_hms("2023-10-11 23:59:00", tz = "MST7MDT"))
-
-calibration_20231010_CO2 <- calibration_20231010 %>%
-  filter(!is.na(calib_CO2_ppm))
-
-calibration_20231010_CO <- calibration_20231010 %>%
-  filter(!is.na(calib_CO_ppm))
-
-CO2_20231010 <- lm(calib_CO2_ppm ~ CO2_.ppm., data = calibration_20231010_CO2)
-summary(CO2_20231010)$coefficients
-
-d <- expand.grid(CO2_.ppm. = seq(0, 1500), calib_CO2_ppm  = c(408, 1008, 2165))
-d$yhat <- predict(CO2_20231010, newdata = d)
-
-p <- ggplot(calibration_20231010_CO2, aes(x = CO2_.ppm., y = calib_CO2_ppm )) + 
-  theme_minimal() + geom_point() + geom_line(aes(y = yhat), data = d) 
-plot(p)
-
-CO_20231010 <- lm(calib_CO_ppm ~ CO_raw.ppm., data = calibration_20231010_CO)
-summary(CO_20231010)$coefficients
-
-d <- expand.grid(CO_raw.ppm. = seq(0, 600), calib_CO_ppm  = c(32, 260, 560))
-d$yhat <- predict(CO_20231010, newdata = d)
-
-p <- ggplot(calibration_20231010_CO, aes(x = CO_raw.ppm., y = calib_CO_ppm )) + 
-  theme_minimal() + geom_point() + geom_line(aes(y = yhat), data = d) 
-plot(p)
-
-########################## End of calibration equations
 #Calibrating CO/CO2 data
 #---------------------------------------------------------------------------------------
 
@@ -311,25 +196,18 @@ UI_TC_bcorr <- UI_TC %>%
 FASMEE_corr_CO_CO2_mean <- left_join(FASMEE_corr_CO_CO2_mean, UI_TC_bcorr, by = c("Sample"="Sample.ID", "SampleType"))
 
 FASMEE_corr_CO_CO2_mean <- FASMEE_corr_CO_CO2_mean %>%
-  mutate(Gas_TC_Carbon_mg.m3 = Mean_CO2_CO_mg.m3 + bcorr_TC_mg.m3)
+  mutate(
+    Gas_TC_Carbon_mg.m3 = Mean_CO2_CO_mg.m3 + bcorr_TC_mg.m3,
+    biomass_mg = Gas_TC_Carbon_mg.m3*2,
+    biomass_g = biomass_mg/1000,
+    biomass_kg = biomass_g/1000)
 
 slim_fasmmee_C <- FASMEE_corr_CO_CO2_mean %>%
-  select(Sample, Day, 3:13, 38:40)
+  select(Sample, 3:13, 40:43) %>%
+  mutate(
+    Sample = as.numeric(Sample)
+  )
 
-############# Emission factors
-
-FASMEE_corr_CO_CO2_mean <- FASMEE_corr_CO_CO2_mean %>%
-  mutate(Sample = as.numeric(Sample))
-
-Spore_EF <- left_join(FASMEE_corr_CO_CO2_mean, spores_bcorr_blue, by = c("Sample"="Sample_num"))
-
-Spore_EF <- Spore_EF %>%
-  mutate(spores.kg = bcorr_spores.m3/biomass_kg)
-
-FASMEE23_microbe_EF <- left_join(Spore_EF, bacteria_bcorr_blue, by = c("Sample"="Sample_num"))
-
-FASMEE23_microbe_EF <- FASMEE23_microbe_EF %>%
-  mutate(bacteria.kg = bcorr_bacteria.m3/biomass_kg)
 
 
 
