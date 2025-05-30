3d81456cfe2c3cf467ab078806b0fa312abfc212
Phin Lampman <plampman7@gmail.com>
2025-05-29 17:45:02 -0700

    Bacteria processing updated for modeling

--- Script/FASMEE23/FASMEE23_bacteria.R
+++ Script/FASMEE23/FASMEE23_bacteria.R
@@ -11,123 +11,149 @@
 #########################################################################
 
 library(tidyverse)
-library(gridExtra)
-library(ggstatsplot)
+
 
 Filter_diam <- 17 ##mm (Obtained from staining tower diameter)
 Filter_area <- ((Filter_diam/2)^2)*pi ##mm^2
-FOV_diam100x <- 0.2 ##mm
-FOV100x_area <- ((FOV_diam100x/2)^2)*pi ##mm^2
-FOV100x.filter <- Filter_area/FOV100x_area ##FoVs/filter
+FOV_diam1000x <- 0.2 ##mm
+FOV1000x_area <- ((FOV_diam1000x/2)^2)*pi ##mm^2
+FOV1000x.filter <- Filter_area/FOV1000x_area ##FoVs/filter
 
-SampleInfo <- read.csv('./Input_Data/FASMEE23/FASMEE_Sample_Info20250329.csv', header = T)
+FOV1000x_40_area = FOV1000x_area * 40
+scaling_1000x = Filter_area/FOV1000x_40_area
 
-SampleInfo <- SampleInfo %>%
-  mutate(DATE_UTC = mdy(DATE_UTC, tz = 'MST7MDT'),
-         DATE_UTC = ymd(DATE_UTC),
-         SampleStart_UTC = ymd_hms(paste(DATE_UTC, SampleStart_UTC), tz = 'UTC'),
-         SampleEnd_UTC = ymd_hms(paste(DATE_UTC, SampleEnd_UTC), tz = 'UTC'),
-         DateTime_MDT = mdy_hm(DateTime_MDT, tz = 'MST7MDT'),
-         DateTime_UTC =  mdy_hm(DateTime_UTC, tz = 'UTC'))
+bacteria <- read.csv('./Input_Data/FASMEE23/FASMEE23_BacteriaCounts_1000X_20250414.csv', header = T)
 
-CellSamples <- SampleInfo %>%
-  filter(FilterType == 'PTFE', SampleType == 'Smoke' | SampleType == 'Ambient') %>%
-  mutate(SampleRep = str_extract(SampleID, ".$"),
-         Sample = str_extract(SampleID, "\\d+"))%>%
-  filter(SampleRep == "B")
-
-blue_info <- SampleInfo %>%
-  filter(Platform == 'Blue',
-         FilterType == 'PTFE') %>%
-  mutate(SampleRep = str_extract(SampleID, ".$"),
-         Sample = str_extract(SampleID, "\\d+")) %>%
-  filter(SampleRep == "A") %>%
-  mutate(int = seq(1, by = 2, length.out = n()))
-
-cells <- read.csv('./Input_Data/FASMEE23/Cell_Counts_FASMEE23_20250414.csv', header = T)
-
-bacteria <- cells %>%
-  filter(StainType == 'S9PI') %>%
+bacteria <- bacteria %>%
   rename(SampleID = 'SlideID') %>%
   mutate(SampleID = if_else(SampleType != "LabBlank" & SampleType != "FieldBlank", gsub("_", "", SampleID), SampleID),
          SampleID = if_else(SampleType == "FieldBlank", gsub("_A", "", SampleID), SampleID))
 
-sample_bacteria <- bacteria %>%
-  filter(Platform == "Blue") %>%
-  group_by(SampleID, Sample, Project, SampleType, StainDate, DateCounted, StainType) %>%
-  summarise(
-    median_bacteria.FOV = median(TotalCells),
-    mean_bacteria.FOV = mean(TotalCells),
-    sd_bacteria.FOV = sd(TotalCells),
-    total_median_bacteria.filter = median_bacteria.FOV*FOV100x.filter,
-    total_mean_bacteria.filter = mean_bacteria.FOV*FOV100x.filter
-  ) %>% ungroup
-
-# sample_bacteria <- left_join(sample_bacteria, SampleInfo, by = c("SampleID", "SampleType"))
-# 
-# sample_bacteria <- sample_bacteria %>%
-#   mutate(
-#     RepVolume_L = Volume_L/2, # A and B slide replicates (S9PI vs CW/KOH)
-#     RepVolume_m3 = RepVolume_L/1000,
-#     Total_bacteria.L = total_bacteria.filter/RepVolume_L,
-#     Total_bacteria.m3 = total_bacteria.filter/RepVolume_m3)
-
-bacteria_LabBlanks <- sample_bacteria %>%
-  filter(SampleType == "LabBlank") %>%
-  group_by(StainDate) %>%
-  summarise(
-    "LabBlank_TotAvg" = mean(total_mean_bacteria.filter)) %>%
-  mutate(StainDate = paste("Blank", StainDate, sep = "_")) %>%
-  pivot_wider(names_from = StainDate, values_from = LabBlank_TotAvg)
-
-bacteria_LBcorr <- sample_bacteria %>%
-  filter(SampleType != "LabBlank") %>% 
-  rowwise() %>%
+bacteria <- bacteria %>%
   mutate(
-    Totalbacteria.filter_LBcorr = as.integer(case_when(
-      StainDate >= 20240503 & StainDate < 20240807 ~ total_mean_bacteria.filter - bacteria_LabBlanks$Blank_20240503,
-      StainDate >= 20240807 & StainDate < 20241119 ~ total_mean_bacteria.filter - bacteria_LabBlanks$Blank_20240807,
-      StainDate >= 20241119 & StainDate < 20241210 ~ total_mean_bacteria.filter - bacteria_LabBlanks$Blank_20241119,
+    LB_Batch = as.factor(case_when(
+      StainDate >= 20240503 & StainDate < 20240807 ~ 'F23A',
+      StainDate >= 20240807 & StainDate < 20241119 ~ 'F23B',
+      StainDate >= 20241119 & StainDate < 20241210 ~ 'F23C',
       ## There were two different TBE solutions with lab blanks from the same stain date, the first set below is TBE1 and second set is TBE2
-      StainDate >= 20241210 & StainDate < 20250205 & SampleID %in% c("R5B", "R4B", "R3B", "R2B", "R1B", "B3B", "B2B") ~ total_mean_bacteria.filter - 0,
-      StainDate >= 20241210 & StainDate < 20250205 & SampleID %in% c("B6B", "R6B", "R3B", "R7B", "R9B", "B9B", "R10B") ~ total_mean_bacteria.filter - 7225,
-      
-      StainDate >= 20250205 ~ total_mean_bacteria.filter - bacteria_LabBlanks$Blank_20250205,
-    ))) %>%
+      StainDate >= 20241210 & StainDate < 20250205 & SampleID %in% 
+        c("LabBlank1A_A_20241210", "R5B", "R4B", "R3B", "R2B", "R1B", "B3B", "B2B") ~ 'F23D',
+      StainDate >= 20241210 & StainDate < 20250205 & SampleID %in% 
+        c("LabBlank3A_A_20241210", "B6B", "R6B", "R3B", "R7B", "R9B", "B9B", "R10B") ~ 'F23E', 
+      StainDate >= 20250205 ~ 'F23F',
+    )))
+
+bacteria <- left_join(bacteria, SampleInfo, by = "SampleID")
+
+bacteria <- bacteria %>%
+  mutate(
+    Volume_L = if_else(is.na(Volume_L), 0, Volume_L),
+    RepVolume_L = Volume_L/2, # A and B slide replicates (S9PI vs CW/KOH)
+    RepVolume_m3 = RepVolume_L/1000) %>%
+  select(-SampleType.y, -Sampler.y) %>%
+  rename(SampleType = 'SampleType.x', Sampler = 'Sampler.x')
+
+blank_means <- bacteria %>%
+  filter(SampleType == "LabBlank") %>%
+  group_by(LB_Batch) %>%
+  summarise(
+    TotalCells_LB = mean(TotalCells),
+    DeadCells_LB = mean(DEADCounts)
+  ) %>%
   ungroup
 
-bacteria_FieldBlanks <- bacteria_LBcorr %>%
-  filter(SampleType == "FieldBlank") %>%
-  #group_by(DATE_UTC) %>%
-  summarise(
-    "FieldBlank_TotAvg" = mean(Tot_bacteria.filter))
+bacteria <- left_join(bacteria, blank_means, by = "LB_Batch")
 
-bacteria_LB_FB_corr <- bacteria_LBcorr %>%
+bacteria <- bacteria %>%
+  filter(SampleType != "LabBlank") %>%
   mutate(
-    Totalbacteria.filter_LB_FBcorr = Totalbacteria.filter_LBcorr - bacteria_FieldBlanks$FieldBlank_TotAvg
+    DeadCells_LBcorr = pmax(0, DEADCounts - DeadCells_LB),
+    TotalCells_LBcorr = pmax(0, TotalCells - TotalCells_LB))
+    
+FieldBlank_mean <- bacteria %>%
+  filter(SampleType == "FieldBlank") %>%
+  summarise(
+    TotalCells_FB = mean(TotalCells_LBcorr),
+    DeadCells_FB = mean(DeadCells_LBcorr)
+  ) %>%
+  ungroup
+
+bacteria <- bacteria %>%
+  filter(SampleType != "FieldBlank") %>%
+  mutate(
+    DeadCells_FBLBcorr = pmax(0, DeadCells_LBcorr - FieldBlank_mean$DeadCells_FB),
+    TotalCells_FBLBcorr = pmax(0, TotalCells_LBcorr - FieldBlank_mean$TotalCells_FB),
+    log_volume_offset_m3 = if_else(SampleType == "Smoke" | SampleType == "Ambient", log(RepVolume_m3), 0))
+
+
+Ambient_mean <- bacteria %>%
+  filter(SampleType == "Ambient") %>%
+  summarise( # Ambient mean for background correction
+    AmbientTotalCells.FOV = mean(TotalCells_FBLBcorr),
+    AmbientDeadCells.FOV = mean(DeadCells_FBLBcorr)
   )
 
-bacteria_LBcorr <- left_join(bacteria_LBcorr, SampleInfo, by = c("SampleID", "SampleType"))
-
-bacteria_LBcorr <- bacteria_LBcorr %>%
+bacteria <- bacteria %>%
   mutate(
-    RepVolume_L = Volume_L/2, # A and B slide replicates (S9PI vs CW/KOH)
-    RepVolume_m3 = RepVolume_L/1000,
-    Tot_bacteria.L = Totalbacteria.filter_LBcorr/RepVolume_L,
-    Tot_bacteria.m3 = Totalbacteria.filter_LBcorr/RepVolume_m3)
+    TotalCells_Bcorr = if_else(SampleType == "Smoke", pmax(0, TotalCells_FBLBcorr - Ambient_mean$AmbientTotalCells.FOV), NA),
+    DeadCells_Bcorr = if_else(SampleType == "Smoke", pmax(0, TotalCells_FBLBcorr - Ambient_mean$AmbientTotalCells.FOV), NA),
+    TotalCells.filter_Bcorr = TotalCells_Bcorr*FOV1000x.filter,
+    DeadCells.filter_Bcorr = DeadCells_Bcorr*FOV1000x.filter,
+    LiveCells.filter_Bcorr = if_else(SampleType == "Smoke", TotalCells.filter_Bcorr - DeadCells.filter_Bcorr, NA),
+    TotalCells_Bcorr.m3 = TotalCells.filter_Bcorr/RepVolume_m3,
+    DeadCells_Bcorr.m3 = DeadCells.filter_Bcorr/RepVolume_m3,
+    LiveCells_Bcorr.m3 = LiveCells.filter_Bcorr/RepVolume_m3,
+    
+    TotalCells.filter_FBLBcorr = TotalCells_FBLBcorr*FOV1000x.filter,
+    DeadCells.filter_FBLBcorr = DeadCells_FBLBcorr*FOV1000x.filter,
+    LiveCells.filter_FBLBcorr = TotalCells.filter_FBLBcorr - DeadCells.filter_FBLBcorr,
+    TotalCells_FBLBcorr.m3 = TotalCells.filter_FBLBcorr/RepVolume_m3,
+    DeadCells_FBLBcorr.m3 = DeadCells.filter_FBLBcorr/RepVolume_m3,
+    LiveCells_FBLBcorr.m3 = LiveCells.filter_FBLBcorr/RepVolume_m3,
+    
+    LiveCells.TotalCells = if_else(LiveCells_FBLBcorr.m3 > 0, LiveCells_FBLBcorr.m3/TotalCells_FBLBcorr.m3, 0)
+    )
 
-bacteria_ambient <- bacteria_LB_FB_corr %>%
-  dplyr::filter(SampleType == "Ambient", Sample != "R6") %>%
-  group_by(SampleID) %>%
-  summarise(bacteria.m3 = mean(Tot_bacteria.m3),
-            sd_bacteria.m3 = sd(Tot_bacteria.m3))
-
-bacteria_bcorr_blue <- bacteria_LB_FB_corr %>%
-  dplyr::filter(SampleType != "Ambient" & SampleType != "FieldBlank", Platform == "Blue") %>%
+bacteria_stat_test <- bacteria %>%
   mutate(
-    bcorr_bacteria.m3 = Tot_bacteria.m3 - bacteria_ambient$bacteria.m3,
-    Sample_num = as.numeric(str_extract_all(Sample, "\\d+"))) %>%
-  select(Sample_num, RepVolume_m3, bcorr_bacteria.m3)
+    SampleID = factor(SampleID),
+    SampleType = factor(SampleType),
+    Platform = factor(Platform),
+    Day = factor(Day),
+    log_volume_offset_m3 = log(RepVolume_m3)) %>% 
+  filter(Platform == "Blue" | (Platform == "Red" & SampleType == "Ambient") | Platform == "Blank")
+
+bacteria_blue <- bacteria_stat_test %>%
+  filter(Platform == "Blue") %>%
+  mutate(Sample_num = if_else(Platform == "Blue", str_extract(Sample, "\\d+"), NA_character_),
+         Sample_num = as.numeric(Sample_num))
+
+bacteria_blue_pa <- left_join(bacteria_blue, PA_stats_FASMEE23, by = c('Sample_num' = 'Sample'))
+
+bacteria_blue_pa_C <- left_join(bacteria_blue_pa, slim_fasmmee_C, by = c('Sample_num' = 'Sample'))
+
+na_count <- bacteria_blue_pa_C %>%
+  summarize(across(everything(), ~sum(is.na(.))))
+
+sample_bacteria_blue <- bacteria_blue_pa_C  %>%
+  group_by(SampleID, SampleType, RepVolume_m3) %>%
+  summarise(
+    RH = mean(MeanRH, na.rm = T),
+    PM2.5 = mean(MedianPM2.5_ug.m3, na.rm = T),
+    Temp = mean(MeanTemp_C, na.rm = T),
+    FOVs = n(),
+    NAFOVs = sum(is.na(FOV)),
+    meanMCE = mean(MeanMCE, na.rm = T),
+    meanlogPM2.5 = mean(logPM2.5, na.rm = T),
+    median_bacteria.FOV = median(TotalCells_FBLBcorr),
+    mean_bacteria.FOV = mean(TotalCells_FBLBcorr),
+    sd_bacteria.FOV = sd(TotalCells_FBLBcorr),
+    total_mean_bacteria.filter = mean(TotalCells.filter_FBLBcorr),
+    total_mean_bacteria.m3 = mean(TotalCells_FBLBcorr.m3),
+    total_mean_bacteria.m3_bcorr = mean(TotalCells_Bcorr.m3),
+    Live.Dead = mean(LiveCells.TotalCells)
+  ) %>% ungroup
+
 
 
 
